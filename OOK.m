e6806c7350cd995575ca29bacaec6b66cf15783d
Kai Jun Ang <angkaijun27@gmail.com>
2021-10-18 16:11:11 +0800

    start of OOK

---
+++ OOK.m
@@ -0,0 +1,9 @@
+function signal = OOK(data)
+%OOK Summary of this function goes here
+%   Detailed explanation goes here
+carrierFreq = 10000;
+carrierSignal = cos(2*pi*carrierFreq);
+signal = data*carrierSignal;
+
+end
+
---
+++ Phase2.m
@@ -0,0 +1,2 @@
+data = randi([0,1], [1,N]);
+signal = OOK(data)
