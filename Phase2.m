11c6f563b04a9cc7651deb7045dc8065f746e0d6
Zhe Ren <zherenlee@gmail.com>
2021-10-21 01:30:56 +0800

    Update Phase2.m

--- Phase2.m
+++ Phase2.m
@@ -1,2 +1,30 @@
+N = 1024;
+dBSNR = 5; %fixed SNR value
+% SNR = 10.^(dBSNR/10);
+% data = generateData(N,dBSNR);
 data = randi([0,1], [1,N]);
-signal = OOK(data)
+
+fc = 10000; %carrier freq
+fs = fc * 16; %sampling freq
+dataRate = 1000;
+
+signalTime = fs*N/dataRate + 1; %WHY PLUS 1?
+[b,a] = butter(6, 0.2); %6th order, 0.2 cutoff freq, lowpassfilter
+freqz(b,a);
+
+t = 0:1/fs:N/dataRate; 
+carrier = 5 .* cos(2*pi*fc*t); %amplitude = 5?
+plot(carrier);
+
+
+dataStream = zeros(1, signalTime);
+for k = 1: signalTime - 1
+    dataStream(k) = data(ceil(k*dataRate/fs));
+end
+dataStream(signalTime) = dataStream(signalTime - 1);
+
+modSignal_OOK = dataStream .* carrier;
+plot(modSignal_OOK);
+xlim([0,100]);
+
+% dataOut = filtfilt(b,a,data);
