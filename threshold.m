8a694c3d51a36a2f31ac874bd84c1d807f4138b3
Kai Jun Ang <angkaijun27@gmail.com>
2021-10-24 14:01:11 +0800

    Removed unnecassary line from threshold

--- threshold.m
+++ threshold.m
@@ -1,7 +1,6 @@
 function thresholdOut = threshold(receivedSig, thresholdValue)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
-N = length(receivedSig);
 
 thresholdOut(receivedSig >= thresholdValue) = 1;
 thresholdOut(receivedSig < thresholdValue) = 0;
