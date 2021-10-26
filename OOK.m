22e038c64ccbdfe7da400db24a091e9bc52f7002
Kai Jun Ang <angkaijun27@gmail.com>
2021-10-26 22:48:56 +0800

    Transferred code into OOK.m

--- OOK.m
+++ OOK.m
@@ -1,9 +1,16 @@
-function signal = OOK(data)
+function signal = OOK(data, fc, dataRate, N)
 %OOK Summary of this function goes here
 %   Detailed explanation goes here
-carrierFreq = 10000;
-carrierSignal = cos(2*pi*carrierFreq);
-signal = data * carrierSignal;
 
+fs = fc * 16; %sampling freq
+t = 0:1/fs:N/dataRate;
+carrier = cos(2*pi*fc*t);
+numSample = fs*N/dataRate + 1;
+dataStream = zeros(1, numSample);
+for k = 1: numSample - 1
+    dataStream(k) = data(ceil(k*dataRate/fs));
+end
+dataStream(numSample) = dataStream(numSample - 1);
+signal = dataStream .* carrier;
 end
 
--- Phase2.m
+++ Phase2.m
@@ -5,26 +5,14 @@ dBSNR = 5; %fixed SNR value
 data = randi([0,1], [1,N]);
 
 fc = 10000; %carrier freq
-fs = fc * 16; %sampling freq
 dataRate = 1000;
 
-signalTime = fs*N/dataRate + 1; %WHY PLUS 1?
-[b,a] = butter(6, 0.2); %6th order, 0.2 cutoff freq, lowpassfilter
-freqz(b,a);
-
-t = 0:1/fs:N/dataRate; 
-carrier = cos(2*pi*fc*t); %amplitude = 5?
-plot(carrier);
-
-
-dataStream = zeros(1, signalTime);
-for k = 1: signalTime - 1
-    dataStream(k) = data(ceil(k*dataRate/fs));
-end
-dataStream(signalTime) = dataStream(signalTime - 1);
-
-modSignal_OOK = dataStream .* carrier;
-plot(modSignal_OOK);
-xlim([0,100]);
+OOK_mod_signal = OOK(data, fc, dataRate, N);
+% numSample = fs*N/dataRate + 1; %WHY PLUS 1?
+% [b,a] = butter(6, 0.2); %6th order, 0.2 cutoff freq, lowpassfilter
+% freqz(b,a);
+% 
+plot(OOK_mod_signal);
+xlim([0,1800]);
 
 % dataOut = filtfilt(b,a,data);
